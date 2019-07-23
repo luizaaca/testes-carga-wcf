69a4a4504ff9932e5c0f5d0e310797e6d090cc1c
Luiz Alberto <luiz.alberto@athosinformatica.local>
2019-07-23 14:38:55 -0300

    Adicionar arquivos de projeto.

---
+++ ClienteTesteCarga/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using ClienteTesteCarga.ServiceReference1;
+
+namespace ClienteTesteCarga
+{
+
+    class Program
+    {
+        private const int numThreads = 100;
+        private static CountdownEvent countdown;
+        private static ManualResetEvent mre = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            string s = null;
+            Console.WriteLine("Press enter to start test.");
+            while ((s = Console.ReadLine()) == string.Empty)
+            {
+                RunTest();
+                Console.WriteLine("Allow a few seconds for threads to die.");
+                Console.WriteLine("Press enter to run again.");
+                Console.WriteLine();
+            }
+        }
+
+        static void RunTest()
+        {
+            mre.Reset();
+            Console.WriteLine("Starting {0} threads.", numThreads);
+            countdown = new CountdownEvent(numThreads);
+            for (int i = 0; i < numThreads; i++)
+            {
+                Thread t = new Thread(ThreadProc);
+                t.Name = "Thread_" + i;
+                t.Start();
+            }
+            // Wait for all threads to signal.
+            countdown.Wait();
+            Console.Write("Press enter to release threads.");
+            Console.ReadLine();
+            Console.WriteLine("Releasing threads.");
+            DateTime startTime = DateTime.Now;
+            countdown = new CountdownEvent(numThreads);
+            // Release all the threads.
+            mre.Set();
+            // Wait for all the threads to finish.
+            countdown.Wait();
+            TimeSpan ts = DateTime.Now - startTime;
+            Console.WriteLine("Total time to run threads: {0}.{1:0}s.",
+                ts.Seconds, ts.Milliseconds / 100);
+        }
+
+        private static void ThreadProc()
+        {
+            Service1Client client = new Service1Client();
+            client.Open();
+            // Signal that this thread is ready.
+            countdown.Signal();
+            // Wait for all the other threads before making service call.
+            mre.WaitOne();
+            client.GetData(1337);
+            // Signal that this thread is finished.
+            countdown.Signal();
+            client.Close();
+        }
+    }
+}
---
+++ TesteCarga/Service1.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading;
+
+namespace TesteCarga
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "Service1" no arquivo de código, svc e configuração ao mesmo tempo.
+    // OBSERVAÇÃO: Para iniciar o cliente de teste do WCF para testar esse serviço, selecione Service1.svc ou Service1.svc.cs no Gerenciador de Soluções e inicie a depuração.
+    public class Service1 : IService1
+    {
+        public Service1()
+        {
+            //ThreadPool.GetMinThreads(out int worker, out int io);
+            //ThreadPool.SetMinThreads(4, 128);
+            //https://devblogs.microsoft.com/aspnet/lets-try-wcf-self-hosted-services-in-a-container/
+        }
+        public string GetData(int value)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            return string.Format("You entered: {0}", value);
+        }
+       
+    }
+}
